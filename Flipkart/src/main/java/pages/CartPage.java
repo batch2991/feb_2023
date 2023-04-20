f65b1fa8798148e05ef8c92e49601ea638dd6d26
venkatg <venkatgn.gutta@gmail.com>
2023-04-20 08:50:39 +0530

    cartpage is updated

--- Flipkart/src/main/java/pages/CartPage.java
+++ Flipkart/src/main/java/pages/CartPage.java
@@ -1,9 +1,15 @@
 package pages;
 
-public class CartPage {
-	public void addProductToCart() {
+public class CartPage 
+{
+	public void addProductToCart() 
+	{
+		System.out.println("code to add a product into cart");
 		
-		
+	}
+	public void removeproductfromcart()
+	{
+		System.out.println("code to remove product from cart");
 	}
 
 }
